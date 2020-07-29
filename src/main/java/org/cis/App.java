093595c77a2b54e32c4b9cf9782976ee5ee8c561
Clu7 <leonardo.guerra@studenti.unibas.it>
2020-07-29 22:15:16 +0200

    Salvataggio parallelo implementato; Creazione struttura cartelle lingua eliminata.

--- src/main/java/org/cis/App.java
+++ src/main/java/org/cis/App.java
@@ -32,44 +32,6 @@ public class App extends Application {
         Applicazione.getInstance().getCommonEvents().moveWindow(root, stage);
         //stage.setResizable(false);
         stage.setTitle("G-Repo");*/
-
-        // C:\Users\ilari\Documents\G-Repo\risorse\cacheCloneRepositories\Jetpack-MVVM-Best-Practice_1
-        /*String toReplace = "risorse" + FileUtils.PATH_SEPARATOR + "cacheCloneRepositories" + FileUtils.PATH_SEPARATOR;
-        Path pathBase = Paths.get(FileUtils.getRootPath());
-
-        Path pathCloneDirectory = Paths.get("C:\\Users\\ilari\\Documents\\G-Repo\\risorse\\cacheCloneRepositories\\Jetpack-MVVM-Best-Practice_1");
-
-        System.out.println("pathCloneDirectory: " + pathCloneDirectory);
-
-        Path pathRelativeCloneDirectory = pathBase.relativize(pathCloneDirectory);
-
-        System.out.println("1. pathRelativeCloneDirectory: " + pathRelativeCloneDirectory);
-
-        pathRelativeCloneDirectory = Paths.get(pathRelativeCloneDirectory.toString().replace(toReplace, ""));
-        String english = "english";
-        if (english.equals("english")) {
-            pathRelativeCloneDirectory = Paths.get("lingua" + "\\" + english + "\\" + pathRelativeCloneDirectory);
-        }
-
-        System.out.println("2. pathRelativeCloneDirectory (Replace): " + pathRelativeCloneDirectory);
-
-        Path pathCloneRepositories = FileUtils.createDirectory(Paths.get("C:\\Users\\ilari\\Desktop\\TOMOVE", "CloneRepositories"));
-
-        // mia radice di salvataggio
-        System.out.println("pathCloneRepositories: " + pathCloneRepositories);
-
-        //Destinazione.
-        Path pathCopy = pathCloneRepositories.resolve(pathRelativeCloneDirectory);
-
-        System.out.println("pathCopy: " + pathCopy);*/
-
-        // no anas
-        // C:\Users\ilari\Desktop\TOMOVE\CloneRepositories\Jetpack-MVVM-Best-Practice_1
-        //2. pathRelativeCloneDirectory (Replace): Jetpack-MVVM-Best-Practice_1
-
-        //si anas
-        // pathCopy: C:\Users\ilari\Desktop\TOMOVE\CloneRepositories\lingua\english\Jetpack-MVVM-Best-Practice_1
-        //2. pathRelativeCloneDirectory (Replace): lingua\english\Jetpack-MVVM-Best-Practice_1
     }
 
     private void initFolder() {
@@ -80,11 +42,11 @@ public class App extends Application {
         FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_CLONING_DIRECTORY));
 
         //## By Language Detection
-        FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_LANGUAGE_REPOSITORIES));
+        /*FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_LANGUAGE_REPOSITORIES));
         FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_ENGLISH));
         FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_MIXED));
         FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_NOT_ENGLISH));
-        FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_UNKNOWN));
+        FileUtils.createDirectory(FileUtils.createAbsolutePath(Constants.RELATIVE_PATH_UNKNOWN));*/
     }
 
     public static void main(String[] args) {
